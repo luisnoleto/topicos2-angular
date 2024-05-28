0ae8189d4dc1360d3da54764cadfb87b58b5f3d3
Luis Noleto <luisnoleto@unitins.br>
2024-05-28 19:51:03 -0300

    alterações

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ToolbarComponent } from '../../components/toolbar/toolbar.component';
+import { ToolbarComponent } from '../toolbar/toolbar.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
