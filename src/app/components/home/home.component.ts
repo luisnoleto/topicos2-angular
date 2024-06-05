6331ae9900466335bfc018499962806401f6643d
Luis Noleto <luisnoleto@unitins.br>
2024-06-05 03:06:35 -0300

    jogocard finalizado

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { ToolbarComponent } from '../template/toolbar/toolbar.component';
 import { RouterOutlet } from '@angular/router';
 import { JogoCardListComponent } from '../jogo-card-list/jogo-card-list.component';
+import { FooterComponent } from '../template/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ToolbarComponent, RouterOutlet, JogoCardListComponent],
+  imports: [
+    ToolbarComponent,
+    RouterOutlet,
+    JogoCardListComponent,
+    FooterComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
